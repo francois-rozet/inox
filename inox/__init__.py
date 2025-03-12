4e04cdb0fd89c3fb4af7ca422a27f179887d249d
François Rozet <francois.rozet@outlook.com>
2025-03-12 10:46:21 +0100

    🔖 Bump version to 0.7.1

--- inox/__init__.py
+++ inox/__init__.py
@@ -1,6 +1,6 @@
 r"""Stainless neural networks in JAX"""
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 
 from . import api, debug, nn, numpy, random, tree  # noqa: F401
 from .api import *  # noqa: F403
